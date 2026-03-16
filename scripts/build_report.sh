8797fefef8ed7177737e93e8d782bc6137faa9a7
kaingan8 <ngankait@gmail.com>
2026-03-16 11:22:44 -0700

    Remove files outside sf6_icp_2d_final folder

--- scripts/build_report.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Compile the LaTeX report.
-# Run from repository root: bash scripts/build_report.sh
-
-cd docs
-pdflatex -interaction=nonstopmode final_report.tex
-pdflatex -interaction=nonstopmode final_report.tex
-pdflatex -interaction=nonstopmode final_report.tex
-echo "Report compiled: docs/final_report.pdf"
